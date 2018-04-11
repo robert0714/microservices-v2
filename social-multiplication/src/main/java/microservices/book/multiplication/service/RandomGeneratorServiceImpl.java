70a907a9e76e3392d9ac66632478755ac0dfe76b
robert.lee <robert0714@gmail.com>
2018-04-11 11:54:45 +0800

    update version

--- social-multiplication/src/main/java/microservices/book/multiplication/service/RandomGeneratorServiceImpl.java
+++ social-multiplication/src/main/java/microservices/book/multiplication/service/RandomGeneratorServiceImpl.java
@@ -12,5 +12,9 @@ class RandomGeneratorServiceImpl implements RandomGeneratorService {
     @Override
     public int generateRandomFactor() {
         return new Random().nextInt((MAXIMUM_FACTOR - MINIMUM_FACTOR) + 1) + MINIMUM_FACTOR;
+        /**
+         * 預設無邏輯回傳為0使促測試程式碼經過assert驗證
+         * **/
+//    	return 0;
     }
 }
